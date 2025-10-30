e43412d901f23c1cf4ffecd16b3cc026bfaa3bac
supriyarajkumar27 <143053218+supriyarajkumar27@users.noreply.github.com>
2025-10-30 12:19:29 +0530

    Update variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,28 +1,40 @@
 variable "rgname" {
   type        = string
-  description = "resource group name"
-
+  description = "Resource group name"
 }
 
 variable "location" {
-  type    = string
-  default = "canadacentral"
+  type        = string
+  description = "Azure region for resources"
+  default     = "centralindia"
 }
 
 variable "service_principal_name" {
-  type = string
+  type        = string
+  description = "Name for the service principal"
 }
 
 variable "keyvault_name" {
-  type = string
+  type        = string
+  description = "Key Vault name"
 }
 
 variable "SUB_ID" {
-  type = string
+  type        = string
+  description = "Azure Subscription ID"
 }
+
 variable "node_pool_name" {
-  
+  type        = string
+  description = "AKS node pool name"
 }
+
 variable "cluster_name" {
-  
+  type        = string
+  description = "AKS cluster name"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "Public SSH key data for Linux VMs in AKS"
 }
